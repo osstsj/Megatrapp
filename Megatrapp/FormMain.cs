c0f740a9018c7fb7bc5c0ac3069ebea1abed2d4d
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2019-05-06 22:03:18 -0500

    Refactor code to fill datagrids into functions and call every interval

--- Megatrapp/FormMain.cs
+++ Megatrapp/FormMain.cs
@@ -20,50 +20,68 @@ namespace Megatrapp
         ZKHelper zKHelper = new ZKHelper();
         private static System.Timers.Timer timer;
 
-        public frmMain()
-        {
+        public frmMain() {
             InitializeComponent();
         }
 
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-
+        private void frmMain_Load(object sender, EventArgs e) {
+            // Download records every 15 minutes or 15,000 miliseconds.
+            // 1000 = 1 second
+            timerApp.Interval = 1000 * 60 * 15;
         }
 
         private void buttonRun_Click(object sender, EventArgs e) {
             try {
                 if (buttonRun.Text == "&Iniciar") {
                     buttonRun.Text = "&Detener";
+                    labelStatus.Text = "En proceso...";
                     timerApp.Enabled = true;
+                    DownloadRecords();
                 } else {
+                    zKHelper.Disconnect();
                     buttonRun.Text = "&Iniciar";
+                    labelStatus.Text = "Detenido";
                     timerApp.Enabled = false;
                 }
-                
-                /*if (zKHelper.ConnectTCP("192.168.0.208", "4370", dataGridViewAttendanceRecords) == 1) {
-                    labelStatus.Text = "Conectado!";
-                    //Application.Run();
-                    List<Employee> employees = zKHelper.GetAllUsersInfo();
-                    List<AttendanceRecord> records = zKHelper.DownloadAttendanceData();
-                    foreach (Employee employee in employees) {
-                        dataGridViewUsers.Rows.Add(employee.EnrollNumber, employee.Name, employee.Privilege, employee.Password, employee.MachineNumber);
-                    }
-                    foreach (AttendanceRecord record in records) {
-                        Employee employee = employees.Find(employeeX => employeeX.EnrollNumber == record.EnrollNumber);
-                        string hour = record.Hour + ":" + record.Minute;
-                        string date = record.Day + "/" + record.Month + "/" + record.Year;
-                        dataGridViewAttendanceRecords.Rows.Add(employee.EnrollNumber, employee.Name, hour, date, record.InOutMode, record.WorkCode);
-                    }
-
-                }*/
             } catch (Exception) {
 
                 throw;
             }
         }
 
+        private void DownloadRecords() {
+            if (zKHelper.ConnectTCP("192.168.0.208", "4370", dataGridViewAttendanceRecords) == 1) {
+                ClearDataGridViewData();
+                labelStatus.Text = "En proceso...";
+                List<Employee> employees = zKHelper.GetAllUsersInfo();
+                List<AttendanceRecord> records = zKHelper.DownloadAttendanceData();
+                FillDataGridEmployees(employees);
+                FillDataGridAttendanceRecords(records, employees);
+                zKHelper.Disconnect();
+            }
+        }
+
+        private void ClearDataGridViewData() {
+            dataGridViewAttendanceRecords.Rows.Clear();
+        }
+
+        private void FillDataGridEmployees(List<Employee> employees) {
+            foreach (Employee employee in employees) {
+                dataGridViewUsers.Rows.Add(employee.EnrollNumber, employee.Name, employee.Privilege, employee.Password, employee.MachineNumber);
+            }
+        }
+
+        private void FillDataGridAttendanceRecords(List<AttendanceRecord> records, List<Employee> employees) {
+            foreach (AttendanceRecord record in records) {
+                Employee employee = employees.Find(employeeX => employeeX.EnrollNumber == record.EnrollNumber);
+                string hour = record.Hour + ":" + record.Minute;
+                string date = record.Day + "/" + record.Month + "/" + record.Year;
+                dataGridViewAttendanceRecords.Rows.Add(employee.EnrollNumber, employee.Name, hour, date, record.InOutMode, record.WorkCode);
+            }
+        }
+
         private void timerApp_Tick(object sender, EventArgs e) {
-            labelStatus.Text = DateTime.Now.ToShortTimeString();
+            DownloadRecords();
         }
     }
 }
